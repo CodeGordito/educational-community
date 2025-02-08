7effe4fb4d4f62c62c5fc3839ad76b070e1af2f9
codeGordito <heykel202018@gmail.com>
2025-02-08 12:46:22 -0300

    Educational Community

---
+++ Comunidad Educativa/Program.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using Comunidad_Educativa;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<MiembroDeLaComunidad> miembros = new List<MiembroDeLaComunidad>
+        {
+            new Administrador("Juan Hernandez", 45, "Subdirector"),
+            new Administrador("Carlos Fernandez", 52, "Gerente de TI"),
+            new Administrador("Marta Garcia", 47, "Jefa de Finanzas"),
+            new Administrador("Pedro Sanchez", 55, "Gerente de RRHH"),
+
+            new Maestro("Claudia Jimenez", 40, "Maestra de Ciencias"),
+            new Maestro("Jose Rodriguez", 45, "Maestro de Historia"),
+            new Maestro("Laura Mejia", 38, "Maestra de Inglés"),
+            new Maestro("Roberto Diaz", 42, "Maestro de Educación Física"),
+
+            new Administrativo("Mariana Torres", 28, "Recepcionista"),
+            new Administrativo("Andres Castillo", 35, "Contador"),
+            new Administrativo("Sofia Rojas", 27, "Analista de Datos"),
+
+            new Estudiante("Carlos Ruiz", 22, "Derecho"),
+            new Estudiante("Maria Gomez", 19, "Psicología"),
+            new Estudiante("Luis Perez", 23, "Arquitectura"),
+
+            new ExAlumno("Miguel Angel", 32, 2010),
+            new ExAlumno("Lucia Mendoza", 29, 2014),
+            new ExAlumno("Fernando Torres", 33, 2008),
+
+            new Empleado("Paola Fernandez", 29, "Gerente de Proyectos"),
+            new Empleado("Sandra Gutierrez", 27, "Especialista en Marketing"),
+            new Empleado("Hugo Marin", 34, "Ingeniero de Software")
+        };
+
+        bool continuar = true;
+
+        while (continuar)
+        {
+            Console.Clear();
+            Console.WriteLine("==================================");
+            Console.WriteLine("      Comunidad Educativa.");
+            Console.WriteLine("==================================");
+            Console.WriteLine(" 1. Agregar Nuevo Miembro");
+            Console.WriteLine(" 2. Mostrar Todos los Miembros");
+            Console.WriteLine(" 3. Buscar Miembro por Nombre");
+            Console.WriteLine(" 4. Mostrar Administradores");
+            Console.WriteLine(" 5. Mostrar Maestros");
+            Console.WriteLine(" 6. Mostrar Administrativos");
+            Console.WriteLine(" 7. Mostrar Estudiantes");
+            Console.WriteLine(" 8. Mostrar ExAlumnos");
+            Console.WriteLine(" 9. Eliminar Miembro");
+            Console.WriteLine("10. Salir");
+            Console.WriteLine("==================================");
+            Console.Write(" Seleccione una opción: ");
+
+            int opcion;
+            if (int.TryParse(Console.ReadLine(), out opcion))
+            {
+                switch (opcion)
+                {
+                    case 1:
+                        AgregarNuevoMiembro(miembros);
+                        break;
+                    case 2:
+                        MostrarTodosLosMiembros(miembros);
+                        break;
+                    case 3:
+                        BuscarMiembroPorNombre(miembros);
+                        break;
+                    case 4:
+                        MostrarMiembros<Administrador>(miembros);
+                        break;
+                    case 5:
+                        MostrarMiembros<Maestro>(miembros);
+                        break;
+                    case 6:
+                        MostrarMiembros<Administrativo>(miembros);
+                        break;
+                    case 7:
+                        MostrarMiembros<Estudiante>(miembros);
+                        break;
+                    case 8:
+                        MostrarMiembros<ExAlumno>(miembros);
+                        break;
+                    case 9:
+                        EliminarMiembro(miembros);
+                        break;
+                    case 10:
+                        continuar = false;
+                        break;
+                    default:
+                        Console.WriteLine("Ingrese una opción válida e intente de nuevo.");
+                        break;
+                }
+                Console.WriteLine("==================================");
+                if (continuar)
+                {
+                    Console.WriteLine("\nPresione cualquier tecla para continuar...");
+                    Console.ReadKey();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ingrese una opción válida e intente de nuevo.");
+                Console.ReadKey();
+            }
+        }
+    }
+
+    static void MostrarMiembros<T>(List<MiembroDeLaComunidad> miembros) where T : MiembroDeLaComunidad
+    {
+        Console.WriteLine($"\n==================================");
+        Console.WriteLine($"{typeof(T).Name}s:");
+        Console.WriteLine("==================================");
+        foreach (var miembro in miembros)
+        {
+            if (miembro is T)
+            {
+                miembro.MostrarInformacion();
+                Console.WriteLine("----------------------------------");
+            }
+        }
+    }
+
+    static void MostrarTodosLosMiembros(List<MiembroDeLaComunidad> miembros)
+    {
+        Console.WriteLine("\n==================================");
+        Console.WriteLine("     Todos los Miembros:");
+        Console.WriteLine("==================================\n");
+        foreach (var miembro in miembros)
+        {
+            miembro.MostrarInformacion();
+            Console.WriteLine("----------------------------------");
+        }
+    }
+
+    static void BuscarMiembroPorNombre(List<MiembroDeLaComunidad> miembros)
+    {
+        Console.WriteLine("\n==================================");
+        Console.Write("Ingrese el nombre del miembro a buscar: ");
+        string nombre = Console.ReadLine();
+        var miembroEncontrado = miembros.Find(m => m.Nombre.Equals(nombre, StringComparison.OrdinalIgnoreCase));
+        if (miembroEncontrado != null)
+        {
+            Console.WriteLine("\nMiembro encontrado:");
+            miembroEncontrado.MostrarInformacion();
+        }
+        else
+        {
+            Console.WriteLine("\nMiembro no encontrado.");
+        }
+    }
+
+    static void AgregarNuevoMiembro(List<MiembroDeLaComunidad> miembros)
+    {
+        Console.WriteLine("==================================");
+        Console.WriteLine("\nSeleccione el tipo de miembro a agregar:");
+        Console.WriteLine("1. Administrador");
+        Console.WriteLine("2. Maestro");
+        Console.WriteLine("3. Administrativo");
+        Console.WriteLine("4. Estudiante");
+        Console.WriteLine("5. ExAlumno");
+        Console.WriteLine("6. Empleado");
+        Console.Write("Seleccione una opción: ");
+        int tipo;
+        if (int.TryParse(Console.ReadLine(), out tipo))
+        {
+            Console.Write("Ingrese el nombre: ");
+            string nombre = Console.ReadLine();
+            Console.Write("Ingrese la edad: ");
+            int edad = int.Parse(Console.ReadLine());
+
+            switch (tipo)
+            {
+                case 1:
+                    Console.Write("Ingrese el puesto: ");
+                    string puestoAdmin = Console.ReadLine();
+                    miembros.Add(new Administrador(nombre, edad, puestoAdmin));
+                    break;
+                case 2:
+                    Console.Write("Ingrese el puesto: ");
+                    string puestoMaestro = Console.ReadLine();
+                    miembros.Add(new Maestro(nombre, edad, puestoMaestro));
+                    break;
+                case 3:
+                    Console.Write("Ingrese el puesto: ");
+                    string puestoAdminis = Console.ReadLine();
+                    miembros.Add(new Administrativo(nombre, edad, puestoAdminis));
+                    break;
+                case 4:
+                    Console.Write("Ingrese la carrera: ");
+                    string carrera = Console.ReadLine();
+                    miembros.Add(new Estudiante(nombre, edad, carrera));
+                    break;
+                case 5:
+                    Console.Write("Ingrese el año de graduación: ");
+                    int anoGraduacion = int.Parse(Console.ReadLine());
+                    miembros.Add(new ExAlumno(nombre, edad, anoGraduacion));
+                    break;
+                case 6:
+                    Console.Write("Ingrese el puesto: ");
+                    string puestoEmpleado = Console.ReadLine();
+                    miembros.Add(new Empleado(nombre, edad, puestoEmpleado));
+                    break;
+                default:
+                    Console.WriteLine("Opción no válida.");
+                    break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Opción no válida.");
+        }
+    }
+
+    static void EliminarMiembro(List<MiembroDeLaComunidad> miembros)
+    {
+        Console.WriteLine("\n==================================");
+        Console.Write("Ingrese el nombre del miembro a eliminar: ");
+        string nombre = Console.ReadLine();
+        var miembroAEliminar = miembros.Find(m => m.Nombre.Equals(nombre, StringComparison.OrdinalIgnoreCase));
+        if (miembroAEliminar != null)
+        {
+            miembros.Remove(miembroAEliminar);
+            Console.WriteLine("\nMiembro eliminado.");
+        }
+        else
+        {
+            Console.WriteLine("\nMiembro no encontrado.");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,42 @@
+# `Educational Community` 🏆
+> [!NOTE]
+"Educational Community" manages members of an educational institution. Developed in C# and .NET, it allows you to view and interact with administrators, teachers, students and alumni through a console interface, using object-oriented programming and lists.
+
+<img src="https://github.com/K3ury99/Comunidad-Educativa/assets/128008789/c92eda80-d5ac-42ce-a486-9c8184d8fab6" width="49%"></img>
+
+# `Features` 🍃
+- **User System**
+  - User registration and authentication.
+  - Customizable user profiles.
+  - Roles and permissions for different user types (administrators, teachers, students).
+
+- **User Interface**
+  - Responsive design for access from different devices.
+  - Intuitive and easy-to-use navigation.
+  - Themes and appearance customization.
+
+- **Security**
+  - Protection of personal data through encryption.
+  - Secure session management.
+  - Clear privacy policies and terms of use.
+
+- **Support and Documentation**
+  - Detailed user guide for students and teachers.
+  - Technical documentation for developers.
+  - Online help and support system.
+
+# `Used technology` ♟
+- C#
+- .NET Framework
+
+# `How to use` 🛠
+Clone the repository to your local machine.
+Open the solution in your preferred IDE (Visual Studio, Visual Studio Code, etc.).
+Compile the project and run the application.
+Follow the instructions on the console to interact with the app and explore the different members of the community.
+
+# `Contributions` 🎈
+Contributions are welcome. If you would like to contribute to this project, please open a pull request with your proposed changes.
+
+# `License` 📔
+This project is licensed under the MIT License.
